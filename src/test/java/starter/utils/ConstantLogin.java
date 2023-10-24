91e64527b869144e6e955bdcbc390acece05d8fb
Ryouta021 <alfiansepty01@gmail.com>
2023-10-25 05:27:13 +0700

    login feature

---
+++ src/test/java/starter/utils/ConstantLogin.java
@@ -0,0 +1,10 @@
+package starter.utils;
+
+public class ConstantLogin {
+    public static final String BASE_URL = "https://mentutor.altapro.online";
+    public static String DIR = System.getProperty("user.dir");
+    public static String JSON = DIR+"/src/test/resources/JSON";
+    public static String REQ_BODY = JSON+"/ReqBody/";
+    public static String JSON_SCHEMA = JSON+"/JSONSchema/";
+
+}
